1d39b91a865d1450dd8c48b8d809093de46e48a8
Daffa Hakim <96603131+Daffahakim23@users.noreply.github.com>
2023-02-26 10:11:06 +0700

    Update segitiga_bintang.dart

--- 07_Branching - Looping - Function/Praktikum/soal_prioritas_2/bin/segitiga_bintang.dart
+++ 07_Branching - Looping - Function/Praktikum/soal_prioritas_2/bin/segitiga_bintang.dart
@@ -9,28 +9,3 @@ void main(List<String> arguments) {
   }
   print(hasil);
 }
-
-// void main() {
-//   int baris = 7;
-//   var hasil = "";
-//   for (int i = 0; i < baris; i++) {
-//     for (int j = 0; j <= i; j++) {
-//       print("* ");
-//     }
-//     print("\n");
-//   }
-// }
-
-// import 'dart:io';
-
-// void main() {
-//   stdout.write('Masukkan sebuah bilangan : ');
-//   int bilangan = int.parse(stdin.readLineSync()!);
-
-//   print('Faktor dari $bilangan adalah: ');
-//   for (int i = 1; i <= bilangan; i++) {
-//     if (bilangan % i == 0) {
-//       print(i);
-//     }
-//   }
-// }
