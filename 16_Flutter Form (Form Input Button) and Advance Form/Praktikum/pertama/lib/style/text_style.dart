a4c57b8f2b469df71efdac7ba792d9d31dc7ed6d
Daffa Hakim <96603131+Daffahakim23@users.noreply.github.com>
2023-03-27 12:21:37 +0700

    Commit Form dan Advance Input

---
+++ 16_Flutter Form (Form Input Button) and Advance Form/Praktikum/pertama/lib/style/text_style.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:pertama/style/colors_style.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TextCustome {
+  TextStyle m3Medium = GoogleFonts.roboto(
+    fontWeight: FontWeight.w500,
+    fontSize: 24,
+    color: ColorStyle().m3Black,
+  );
+
+  TextStyle m3TitleMedium = GoogleFonts.roboto(
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    color: ColorStyle().blue,
+  );
+}
---
+++ 16_Flutter Form (Form Input Button) and Advance Form/praktikum2/lib/style/text_style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TextCustome {
+  TextStyle m3Medium = GoogleFonts.roboto(
+    fontWeight: FontWeight.w700,
+    fontSize: 20,
+  );
+}
