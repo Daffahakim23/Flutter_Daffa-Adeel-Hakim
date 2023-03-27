2ab15a70d41ec5f816ecdab6abf8993224cb6af1
Daffa Hakim <96603131+Daffahakim23@users.noreply.github.com>
2023-03-27 12:24:55 +0700

    Delete 16_Flutter Form (Form Input Button) and Advance Form/praktikum2 directory

--- 16_Flutter Form (Form Input Button) and Advance Form/praktikum2/lib/style/text_style.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-
-class TextCustome {
-  TextStyle m3Medium = GoogleFonts.roboto(
-    fontWeight: FontWeight.w700,
-    fontSize: 20,
-  );
-}
